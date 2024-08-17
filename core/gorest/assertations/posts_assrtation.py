1f9167b5f83dc7289b3f1089a172a96e3a88e98d
mr_crow <merdenis91@gmail.com>
2024-08-17 08:33:52 +0200

    lesson 24. moved  asserts from test

--- core/gorest/assertations/posts_assrtation.py
+++ core/gorest/assertations/posts_assrtation.py
@@ -1,4 +1,4 @@
-from assertpy import assert_that, soft_assertions, soft_fail
+from assertpy import assert_that, soft_assertions
 
 
 def post_was_created(request_data, response_data, user_id):
@@ -12,3 +12,22 @@ def post_was_created(request_data, response_data, user_id):
         response_list_of_keys = [k for k in response_data if k not in ('id', 'user_id')]
 
         assert_that(sorted(request_data), 'response has extra_keys').is_equal_to(sorted(response_list_of_keys))
+
+
+def get_posts_assertations(post_data, response_data, user_id):
+
+    assert_that(len(response_data),
+                f'Expected quantity of posts is more than 1, but actual is {len(response_data)}').is_greater_than(0)
+
+    filtered_post = [k for k in response_data if k.get('id') == post_data.get('id')]
+
+    assert_that(filtered_post).is_length(1)
+    filtered_post = filtered_post[0]
+
+    with soft_assertions():
+        assert_that(filtered_post['id']).is_not_none()
+        assert_that(filtered_post['user_id']).is_equal_to(user_id)
+        assert_that(filtered_post['title']).is_equal_to(post_data['title'])
+        assert_that(filtered_post['body']).is_equal_to(post_data['body'])
+
+        assert_that(sorted(post_data), 'response has extra_keys').is_equal_to(sorted(filtered_post))
--- tests/test_gorest/test_user_posts/test_get_user_posts.py
+++ tests/test_gorest/test_user_posts/test_get_user_posts.py
@@ -1,11 +1,5 @@
-from pytest import mark
-from core.gorest.assertations.user_asserations import assert_user_was_created_response
-from core.gorest.utils.user_enums import Statuses, Gender
-from tests.test_gorest.conftest import BaseUser
+from core.gorest.assertations.posts_assrtation import get_posts_assertations
 from tests.test_gorest.test_user_posts.conftest import GorestUserPosts
-from tests.test_gorest.test_users.conftest import GorestCreateUserBase
-from utils.faker_utils import faker
-from assertpy import soft_assertions, assert_that
 
 
 class TestGetUserPost(GorestUserPosts):
@@ -13,24 +7,9 @@ class TestGetUserPost(GorestUserPosts):
     def test_get_user_posts(self, created_user, created_post):
         user_id = created_user.get('id')
 
-        posts = self.gorest_ctrl.get_user_posts(user_id=user_id).json()
+        posts = self.gorest_ctrl.get_user_posts(user_id=user_id)
 
-        assert_that(len(posts),
-                    f'Expected quantity of posts is more than 1, but actual is {len(posts)}').is_greater_than(0)
-
-        filtered_post = [k for k in posts if k.get('id') == created_post.get('id')]
-
-        assert_that(filtered_post).is_length(1)
-        filtered_post = filtered_post[0]
-
-        with soft_assertions():
-            assert_that(filtered_post['id']).is_not_none()
-            assert_that(filtered_post['user_id']).is_equal_to(user_id)
-            assert_that(filtered_post['title']).is_equal_to(created_post['title'])
-            assert_that(filtered_post['body']).is_equal_to(created_post['body'])
-
-
-            assert_that(sorted(created_post), 'response has extra_keys').is_equal_to(sorted(filtered_post))
+        get_posts_assertations(post_data=created_post, response_data=posts.json(), user_id=user_id)
 
 
 
